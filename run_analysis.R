da8c5d196293e60d173c991dd563b0f4fb357272
omah-un <brianmorrisdevelopment@gmail.com>
2014-04-16 16:17:41 -0500

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -34,5 +34,9 @@ for (i in 1:6){
 }
 
 #Part 5: Creates a second, independent tidy data set with the average of each variable for each activity and each subject. 
-tidySet <- aggregate(. ~ Label+Subject, data = result, mean)
-write.table(means, file="tidySet.txt", row.names=FALSE)
+tidySet <- aggregate(. ~ Label+Subject, data = merger, mean)
+write.table(tidySet, file="tidySet.txt", row.names=FALSE)
+
+result$Subject
+result$Label
+
