6e1c7d5f0ee51ecf9dec86b0f4f708027c7293f3
omah-un <brianmorrisdevelopment@gmail.com>
2014-04-16 16:04:50 -0500

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -1,26 +1,18 @@
 #Part 1: Merges the training and the test sets to create one data set.
-# Set the URLs
-trainSetUrl <- "./UCI HAR Dataset/train/X_train.txt"
-trainLabelUrl <- "./UCI HAR Dataset/train/y_train.txt"
-trainSubjectUrl <- "./UCI HAR Dataset/train/subject_train.txt"
-testSetUrl <- "./UCI HAR Dataset/test/X_test.txt"
-testLabelUrl <- "./UCI HAR Dataset/test/y_test.txt"
-testSubjectUrl <- "./UCI HAR Dataset/test/subject_test.txt"
-featuresUrl <- "./UCI HAR Dataset/features.txt"
 
 #Read the tables from train data and put into one DF
-features <- read.table(featuresUrl, header=F)
-trainSet <- read.table(trainSetUrl, header=F)
+features <- read.table("./UCI HAR Dataset/features.txt")
+trainSet <- read.table("./UCI HAR Dataset/train/X_train.txt")
 colnames(trainSet) <- features$V2
-trainLabel <- read.table(trainLabelUrl, header=F)
-trainSubject <- read.table(trainSubjectUrl, header=F)
+trainLabel <- read.table("./UCI HAR Dataset/train/y_train.txt")
+trainSubject <- read.table("./UCI HAR Dataset/train/subject_train.txt")
 train <- cbind(trainSet,trainLabel, trainSubject)
 
 #Read the tables from test data and put into one DF
-testSet <- read.table(testSetUrl, header=F)
+testSet <- read.table("./UCI HAR Dataset/test/X_test.txt")
 colnames(testSet) <- features$V2
-testLabel <- read.table(testLabelUrl, header=F)
-testSubject <- read.table(testSubjectUrl, header=F)
+testLabel <- read.table("./UCI HAR Dataset/test/y_test.txt")
+testSubject <- read.table("./UCI HAR Dataset/test/subject_test.txt")
 test <- cbind(testSet, testLabel, testSubject)
 
 #Merge DF
@@ -34,8 +26,7 @@ extractedValues <- mergedData[c(extractedColumns, 562,563)]
 #Part 3: Uses descriptive activity names to name the activities in the data set
 colnames(mergedData)[562] <- "Label"
 colnames(mergedData)[563] <- "Subject"
-activityLabels <- c("walking", "walking upstairs", "walking downstairs",
-              "sitting", "standing", "laying")
+activityLabels <- c("walking", "walking upstairs", "walking downstairs","sitting", "standing", "laying")
 
 #Part 4: Appropriately labels the data set with descriptive activity names. 
 for (i in 1:6){
@@ -45,3 +36,8 @@ for (i in 1:6){
 #Part 5: Creates a second, independent tidy data set with the average of each variable for each activity and each subject. 
 tidySet <- aggregate(. ~ Label+Subject, data = extractedValues, mean)
 write.table(means, file="tidySet.txt", row.names=FALSE)
+
+
+
+
+
